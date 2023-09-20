533da23e6453e1ae8c86ec460b7299214d034445
Jewel Hossain Fahim <76914444+JewelHFahim@users.noreply.github.com>
2023-09-20 17:30:10 +0600

    done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,8 @@
 import logo from "../../assets/logo.svg";
 import search from "../../assets/searchicon.svg";
-import "./Navbar.css"
+import "./Navbar.css";
 
 export default function Navbar() {
-
   const navigation = [
     { title: "Home" },
     { title: "Shoes" },
@@ -13,24 +12,26 @@ export default function Navbar() {
 
   return (
     <nav className="w-full px-[150px]">
-      
-
-      <div className="flex  justify-between items-center pt-[30px] pr-4">
-
-        <a href="" className="flex items-center w-[165px] h-[90px]">
-
-          <h1 className="text-[27px] font-[900] text-white uppercase">Micro</h1>
-
-          <span className="ml-[-18px]">
-            <img src={logo} alt="" width={59} height={59} className="ml-[18px]"/>
-            <h1 className="text-[27px] font-[900] text-white uppercase">
-              Shoe
+      <div className="flex justify-between items-center pt-[30px] pr-4">
+        <a href="" className="flex items-end gap-2 w-[165px] h-[90px]">
+          <h1 className="text-[27px] font-[900] text-[#cf9c5d] uppercase micro">
+            Micro
+          </h1>
+          <span className="logo">
+            <img
+              src={logo}
+              alt=""
+              width={59}
+              height={59}
+              className="ml-[18px]"
+            />
+            <h1 className="text-[27px] font-[900] text-[#cf9c5d] uppercase shoe">
+              Shoes
             </h1>
           </span>
         </a>
 
         <div className="flex space-x-[80px]">
-
           <ul className="flex justify-center items-center space-x-[80px]">
             {navigation.map((item, idx) => {
               return (
@@ -45,14 +46,14 @@ export default function Navbar() {
           </ul>
 
           <div className="w-[290px] h-[34px] rounded-[48px] border border-[#CF9C5D] flex justify-between px-2 items-center serachbar">
-            <input type="text" className="w-[260px] h-[34px] rounded-[48px] bg-transparent focus:outline-none pl-2 text-white"/>
+            <input
+              type="text"
+              className="w-[260px] h-[34px] rounded-[48px] bg-transparent focus:outline-none pl-2 text-white"
+            />
             <img src={search} alt="" width={24} height={24} />
           </div>
-
         </div>
-
       </div>
-      
     </nav>
   );
 }
--- src/components/slider/MainSlider2.jsx
+++ src/components/slider/MainSlider2.jsx
@@ -4,61 +4,51 @@ import image1 from "../../assets/apex s1.png";
 import image2 from "../../assets/bata.png";
 import image3 from "../../assets/lotto.png";
 import "./MainSlider.css";
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
-
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 const MainSlider2 = () => {
+  const datas = [
+    {
+      img: image1,
+      classStrock: "apex-strock",
+      classFill: "apex-text",
+    },
+    {
+      img: image2,
+      classStrock: "bata-strock",
+      classFill: "bata-text",
+    },
+    {
+      img: image3,
+      classStrock: "lotto-strock",
+      classFill: "lotto-text",
+    },
+  ];
+
   return (
     <Carousel
-      showArrows={true} 
-      showThumbs={true} 
+      showThumbs={true}
       swipeable={true}
-      // autoPlay={true}
+      autoPlay={true}
       infiniteLoop={true}
       fadeIn={true}
       showStatus={false}
-      interval={5000}
+      useKeyboardArrows={true}
     >
-
-      <div className="slide1 relative carousel-slide">
-        <img src={image1} alt="Slide 1" className="z-[10]" />
-        <div className="apex-text"></div>
-        <div className="strock-img"></div>
-        <div className="flex absolute top-[62%] right-[180px] text-xl gap-1 text-[#cf9c5d]">
-          <AiFillStar/> 
-          <AiFillStar/> 
-          <AiFillStar/> 
-          <AiFillStar/>
-          <AiOutlineStar/>
+      {datas.map((data, i) => (
+        <div key={i}>
+          <img src={data.img} alt="Slide 1" className="z-[10]" />
+          <div className={data.classFill}></div>
+          <div className={data.classStrock}></div>
+          <div className="flex absolute top-[61.5%] right-[180px] text-xl gap-1 text-[#cf9c5d]">
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiOutlineStar />
           </div>
-      </div>
-
-      <div className="slide1 relative carousel-slide">
-        <img src={image2} alt="Slide 1" className="z-[10]" />
-        <div className="apex-text"></div>
-        <div className="strock-img"></div>
-        <div className="flex absolute top-[62%] right-[180px] text-xl gap-1 text-[#cf9c5d]">
-          <AiFillStar/> 
-          <AiFillStar/> 
-          <AiFillStar/> 
-          <AiFillStar/>
-          <AiOutlineStar/>
-          </div>
-      </div>
-
-      <div className="slide1 relative carousel-slide">
-        <img src={image3} alt="Slide 1" className="z-[10]" />
-        <div className="apex-text"></div>
-        <div className="strock-img"></div>
-        <div className="flex absolute top-[62%] right-[180px] text-xl gap-1 text-[#cf9c5d]">
-          <AiFillStar/> 
-          <AiFillStar/> 
-          <AiFillStar/> 
-          <AiFillStar/>
-          <AiOutlineStar/>
-          </div>
-      </div>
-
+        </div>
+      ))}
     </Carousel>
   );
 };
